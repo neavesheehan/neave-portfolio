564b2615e9eeec14f2038470a8780152d756d783
Neave Sheehan <neavemegansheehan@outlook.com>
2026-01-26 10:12:45 +0000

    Add Vercel Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import Navigation from "@/components/Navigation";
 import Footer from "@/components/Footer";
+import { Analytics } from "@vercel/analytics/react"
 
 export const metadata: Metadata = {
   title: "Neave Sheehan | AI Engineer & Data Analyst",
@@ -20,6 +21,7 @@ export default function RootLayout({
         <Navigation />
         <main>{children}</main>
         <Footer />
+        <Analytics />
       </body>
     </html>
   );
